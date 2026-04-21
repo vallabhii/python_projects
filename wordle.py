ded2777baee35bd22e851d713567551ea4f8f0d3
vallabhii <vallabhiich@gmail.com>
2026-04-21 15:25:18 +0530

    Add Wordle game implementation in wordle.py
    Implemented a Wordle-like game where players guess a 5-letter word with feedback on their guesses. Includes functionality for replaying the game and validating word existence and length.

---
+++ wordle.py
@@ -0,0 +1,49 @@
+from random_word import RandomWords
+from wordfreq import zipf_frequency
+r = RandomWords()
+playagain=""
+while playagain!="n":
+    word=""
+    print("Lets play wordl!\nThe word is 5 alphabets long,\n🟩 Indicates that the alphabet is correct and in the right place,\n🟨 Indicates that the alphabet is in the word but in the wrong place,\n⬜️ Indicates that the alphabet is not in the word at all.\nYou get 7 attempts to guess the word!")
+    while not word or len(word)!=5 or zipf_frequency(word, 'en')<=0:
+        word = r.get_random_word()
+    guess=""
+    round=7
+    while guess!=word and round!=0:
+        wordlis=list(word)
+        g=input("Guess the word: ")
+        guess=g.lower()
+        ans=[" ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "," ⬜️ "]
+        if zipf_frequency(guess, 'en')>0 and len(guess)==5:
+            for i in range(5):
+                print(" ",guess[i],end=" ")
+            print("")
+            for i in range(5):
+                if guess[i]==word[i] and guess[i] in wordlis:
+                    ans[i]=" 🟩 "
+                    wordlis.pop(wordlis.index(guess[i]))
+            for i in range(5):
+                if guess[i]!=word[i] and guess[i] in wordlis:
+                    ans[i]=" 🟨 "
+                    wordlis.pop(wordlis.index(guess[i]))
+            for i in range(5):
+                print(ans[i],end="")
+            print("")
+            round-=1
+        elif zipf_frequency(guess, 'en')>0 and len(guess)!=5:
+            print("This word is not 5 alphabets, please enter a valid word.")
+        elif zipf_frequency(guess, 'en')<=0 and len(guess)==5:
+            print("This word does not exist, please enter a valid word.")
+        elif zipf_frequency(guess, 'en')<=0 and len(guess)!=5:
+            print("This word does not exist, nor is it 5 alphabets, please enter a valid word.")
+    if guess==word:
+        print("Yayyyyy! You got it in ",7-round," tries!")
+    else:
+        print("Awn man, better luck next time!\nThe word was\'",word,"\'")
+    a=0
+    while a!=1:
+        playagain=input("Would you like to play again? Enter \'y\' for yes and \'n\' for no: ")
+        if playagain=="y" or playagain=="n":
+            a=1
+        else:
+            print("Please enter a valid response.")
