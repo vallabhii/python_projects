fd3428d1deafb5b8703838f0b315c115992b340f
Vishnu Vallabhi Chintala <vallabhiich@gmail.com>
2026-04-26 14:41:40 +0530

    Create mlattempt1.py

---
+++ mlattempt1.py
@@ -0,0 +1,130 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import math
+
+def compute_cost_fxn(x,y,w,b):
+    m=x.shape[0]
+    cost=0.0
+    for i in range(m):
+        f_wb=np.dot(w,x[i])+b
+        cost+=(f_wb-y[i])**2
+    cost=cost/(2*m)
+    return(cost)
+
+def compute_gradient(x,y,w,b):
+    m=x.shape[0]
+    dj_dw=np.zeros_like(w)
+    dj_db=0.0
+    for i in range(m):
+        f_wb=np.dot(w,x[i])+b
+        dj_dw+=(f_wb-y[i])*x[i]
+        dj_db+=f_wb-y[i]
+    dj_dw=dj_dw/m
+    dj_db=dj_db/m     
+    return dj_dw,dj_db
+
+def gradient_descent(x,y,w_in,b_in,cost_function,gradient_function,alpha,num_iterations):
+    m=x.shape[0]
+    J_history=[]
+    w_history=[]
+    w=copy.deepcopy(w_in)
+    b=b_in
+    for i in range(num_iterations):
+        dj_dw, dj_db = gradient_function(x,y,w,b)
+        w=w-alpha*dj_dw               
+        b=b-alpha*dj_db 
+        if i<num_iterations: 
+            cost=cost_function(x,y,w,b)
+            J_history.append(cost)
+        step = max(1, num_iterations // 10)
+        if i % step == 0:
+            w_history.append(w.copy())
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}")     
+    return w,b,J_history,w_history
+import matplotlib.pyplot as plt
+import copy
+import math
+
+def compute_cost_fxn(x,y,w,b):
+    m=x.shape[0]
+    cost=0.0
+    for i in range(m):
+        f_wb=np.dot(w,x[i])+b
+        cost+=(f_wb-y[i])**2
+    cost=cost/(2*m)
+    return(cost)
+
+def compute_gradient(x,y,w,b):
+    m=x.shape[0]
+    dj_dw=np.zeros_like(w)
+    dj_db=0.0
+    for i in range(m):
+        f_wb=np.dot(w,x[i])+b
+        dj_dw+=(f_wb-y[i])*x[i]
+        dj_db+=f_wb-y[i]
+    dj_dw=dj_dw/m
+    dj_db=dj_db/m     
+    return dj_dw,dj_db
+
+def gradient_descent(x,y,w_in,b_in,cost_function,gradient_function,alpha,num_iterations):
+    m=x.shape[0]
+    J_history=[]
+    w_history=[]
+    w=copy.deepcopy(w_in)
+    b=b_in
+    for i in range(num_iterations):
+        dj_dw, dj_db = gradient_function(x,y,w,b)
+        w=w-alpha*dj_dw               
+        b=b-alpha*dj_db 
+        if i<num_iterations: 
+            cost=cost_function(x,y,w,b)
+            J_history.append(cost)
+        step = max(1, num_iterations // 10)
+        if i % step == 0:
+            w_history.append(w.copy())
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}")     
+    return w,b,J_history,w_history
+import matplotlib.pyplot as plt
+import copy
+import math
+
+def compute_cost_fxn(x,y,w,b):
+    m=x.shape[0]
+    cost=0.0
+    for i in range(m):
+        f_wb=np.dot(w,x[i])+b
+        cost+=(f_wb-y[i])**2
+    cost=cost/(2*m)
+    return(cost)
+
+def compute_gradient(x,y,w,b):
+    m=x.shape[0]
+    dj_dw=np.zeros_like(w)
+    dj_db=0.0
+    for i in range(m):
+        f_wb=np.dot(w,x[i])+b
+        dj_dw+=(f_wb-y[i])*x[i]
+        dj_db+=f_wb-y[i]
+    dj_dw=dj_dw/m
+    dj_db=dj_db/m     
+    return dj_dw,dj_db
+
+def gradient_descent(x,y,w_in,b_in,cost_function,gradient_function,alpha,num_iterations):
+    m=x.shape[0]
+    J_history=[]
+    w_history=[]
+    w=copy.deepcopy(w_in)
+    b=b_in
+    for i in range(num_iterations):
+        dj_dw, dj_db = gradient_function(x,y,w,b)
+        w=w-alpha*dj_dw               
+        b=b-alpha*dj_db 
+        if i<num_iterations: 
+            cost=cost_function(x,y,w,b)
+            J_history.append(cost)
+        step = max(1, num_iterations // 10)
+        if i % step == 0:
+            w_history.append(w.copy())
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}")     
+    return w,b,J_history,w_history
