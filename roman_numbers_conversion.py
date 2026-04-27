46437407897953c64d2ac397617067db0fb96043
Vishnu Vallabhi Chintala <vallabhiich@gmail.com>
2026-04-27 12:20:56 +0530

    Update roman_numbers_conversion.py

--- roman_numbers_conversion.py
+++ roman_numbers_conversion.py
@@ -3,12 +3,21 @@ valid_sub={"I":["V","X"],"X":["L","C"],"C":["D","M"]}
 print("Converting roman number into integer.")
 error=False
 while error!=True:  
-    a=input("Enter roman number:")
+    a=input("Enter roman number: ")
     b=a.capitalize()
     number=[]
     for i in range(len(b)):
         if b[i] not in list(symbols.keys()):
             print(b[i], " does not exist in the roman number system.")
         elif b[i] in list(valid_sub.keys()) and b[i+1] not in valid_sub[b[i]]:
-            print("I can be subtracted  from V and X, X can be subtracted from L and C and, C can be subtracted from D and M\nso, ",b[i-1]," cannot be subtracted from ",b[i],".")
+            print("I can be subtracted  from V and X, X can be subtracted from L and C and, C can be subtracted from D and M.\nSo, ",b[i-1]," cannot be subtracted from ",b[i],".")
+        elif [b[i+1],b[i+2],b[i+3],b[i+4]]==[b[i],b[i],b[i],b[i]]:
+            print("Each symbol cannot be used more than 3 times consecutively.\nYou used ",b[i]," more than 3 times consecutively")
         else:
+            number.append(b[i])
+    if len(number)==len(b):
+        error=False
+    else:
+        print("Please enter a valid roman number.")
+    
+
