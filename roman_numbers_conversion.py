7f6f9604335e88c049c433b45615b9bfb5bd97eb
Vishnu Vallabhi Chintala <vallabhiich@gmail.com>
2026-04-29 23:09:27 +0530

    Update roman_numbers_conversion.py

--- roman_numbers_conversion.py
+++ roman_numbers_conversion.py
@@ -1,6 +1,7 @@
 symbols={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
 subs={"IV":4,"IX":9,"XL":40,"XC":90,"CD":400,"CM":900}
 hundereds_sub,tens_sub,ones_sub=False,False,False
+ones,tens,hundereds=0,0,0
 int_num_list=[]
 invalid_rom_num=False
 a=input("Enter roman number")
@@ -15,21 +16,40 @@ for i in range(len(rom_num_list)):
         invalid_rom_num=True
         break
 if invalid_rom_num==False:
-    print("gi")
-    for j in range(1,len(rom_num_values)):
-        if rom_num_values[j-1]<rom_num_values[j]: 
-            if rom_num_list[j-1]+rom_num_list[j] in list(subs.keys()):
-                rom_num_values[j]=subs[rom_num_list[j-1]+rom_num_list[j]]
-                rom_num_list[j]=rom_num_list[j-1]+rom_num_list[j]
-                rom_num_list.pop(j-1)
-                rom_num_values.pop(j-1)
-                j-=1
-                if rom_num_values[j]<10:
+    length=len(rom_num_values)
+    a=0
+    while a<length-1:
+        a+=1
+        if rom_num_values[a-1]<rom_num_values[a]: 
+            if rom_num_list[a-1]+rom_num_list[a] in list(subs.keys()):
+                rom_num_values[a]=subs[rom_num_list[a-1]+rom_num_list[a]]
+                rom_num_list[a]=rom_num_list[a-1]+rom_num_list[a]
+                rom_num_list.pop(a-1)
+                rom_num_values.pop(a-1)
+                a-=1
+                length-=1
+                if rom_num_values[a]<10:
                     ones_sub=True
-                elif rom_num_values[j]<100:
+                elif rom_num_values[a]<100:
                     tens_sub=True
-                elif rom_num_values[j]<1000:
+                elif rom_num_values[a]<1000:
                     hundereds_sub=True
             else:
                 invalid_rom_num=True
-                
+                a=length-1
+                break
+    
+if invalid_rom_num==False:               
+    for j in range(len(rom_num_values)):
+        if rom_num_values[j]<10:
+            ones+=1
+        elif rom_num_values[j]<100:
+            tens+=1
+        elif rom_num_values[j]<1000:
+            hundereds+=1
+    if (ones_sub and ones>1) or (tens_sub and tens>1) or (hundereds_sub and hundereds>1):
+        invalid_rom_num=True
+if invalid_rom_num==False:
+    print(sum(rom_num_values))
+else:
+    print("invalid roman number")
